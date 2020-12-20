5d9944bc70e5c272afd96bc60c9aefc2700e71a6
Hedcler Morais <hedcler@Hedclers-MacBook-Pro.local>
2020-12-20 12:34:55 -0300

    Add not nullable and foreing keys constraints and also reorder query executions

--- create_tables.py
+++ create_tables.py
@@ -46,18 +46,6 @@ def create_tables(cur, conn):
 
 
 def main():
-    """
-    - Drops (if exists) and Creates the sparkify database. 
-    
-    - Establishes connection with the sparkify database and gets
-    cursor to it.  
-    
-    - Drops all the tables.  
-    
-    - Creates all tables needed. 
-    
-    - Finally, closes the connection. 
-    """
     cur, conn = create_database()
     
     drop_tables(cur, conn)
